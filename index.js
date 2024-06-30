abc145a6a78464c4102a2e55186c85d23e30a4ca
Duncan <njorogedun870@gmail.com>
2024-06-30 20:12:48 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+let companyName = "scuber";
+
+const comapanyNameNew = "Safaricom";
+
+let mostProfitableNeighborhood = "Chelsea";
+
+const mostProfitableNeighborhood2 = "Chelsea";
+
+let companyCeo = "Susan Smith";
+
+const companyCeo2 = "Susan Smith";
